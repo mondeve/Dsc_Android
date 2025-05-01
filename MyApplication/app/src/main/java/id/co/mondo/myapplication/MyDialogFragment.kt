b8f40eb6a785c642cd85d0ac078bfbf8d88ea80b
Raqhib <raqhib.ataillah@gmail.com>
2025-05-01 15:31:25 +0800

    Bottom Navigation

---
+++ MyApplication/app/src/main/java/id/co/mondo/myapplication/MyDialogFragment.kt
@@ -0,0 +1,31 @@
+package id.co.mondo.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import id.co.mondo.myapplication.databinding.FragmentDialogBinding
+
+class MyDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View?
+    {
+        _binding = FragmentDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.textViewJudul.text = "ADAKAH SERATUS"
+
+    }
+
+}
